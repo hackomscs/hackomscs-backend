d7608eaec841095776aebc41dc8d33ef88916718
Pham, Danh T <danhpham351@gmail.com>
2023-10-11 18:48:40 -0400

    spring backend boilerplate

---
+++ src/main/java/com/hackomscs/hackomscsbackend/HackomscsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackomscs.hackomscsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackomscsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackomscsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackomscs/hackomscsbackend/HackomscsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackomscs.hackomscsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackomscsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
