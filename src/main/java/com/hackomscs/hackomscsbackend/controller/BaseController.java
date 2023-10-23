2ce9a6adcb28c657a21dc6ba0e1281d67e820a2f
Pham, Danh T <danhpham351@gmail.com>
2023-10-23 16:56:50 -0400

    add dockerfile for build

--- src/main/java/com/hackomscs/hackomscsbackend/controller/BaseController.java
+++ src/main/java/com/hackomscs/hackomscsbackend/controller/BaseController.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 @RestController
 public class BaseController {
 
-    @Autowired
     private TokenService tokenService;
 
     @GetMapping("/getSessionToken")
