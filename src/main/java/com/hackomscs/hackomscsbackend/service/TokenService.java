c0913dfa4b66fb41472d0e09f8e3488f63bc679b
Pham, Danh T <danhpham351@gmail.com>
2023-10-23 17:33:17 -0400

    refactor TokenService to use environment variables

--- src/main/java/com/hackomscs/hackomscsbackend/service/TokenService.java
+++ src/main/java/com/hackomscs/hackomscsbackend/service/TokenService.java
@@ -1,37 +1,26 @@
 package com.hackomscs.hackomscsbackend.service;
 
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class TokenService {
 
-
     private final RestTemplate restTemplate = new RestTemplate();
 
-    // TODO: put secret in env variables
-    // rn it is in application.properties
+    // GCloud env variables
+    private String clientId = System.getenv("MYMLH_CLIENT_ID");
+    private String clientSecret = System.getenv("MYMLH_CLIENT_SECRET");
+    private String redirectUri = System.getenv("MYMLH_REDIRECT_URI");
 
-    @Value("${mymlh.client.id}")
-    private String clientId;
-
-    @Value("${mymlh.client.secret}")
-    private String clientSecret;
-
-    @Value("${mymlh.redirect.uri}")
-    private String redirectUri;
-
-    // step 3 in https://my.mlh.io/docs#oauth_flows
     public String getAccessToken(String authCode) {
         return restTemplate.postForObject(
-            String.format(
-                "https://my.mlh.io/oauth/token?client_id=%s&client_secret=%s&code=%s&grant_type=authorization_code&redirect_uri=%s", 
-                clientId, clientSecret, authCode, redirectUri
-            ),
-            null,
-            String.class
+                String.format(
+                        "https://my.mlh.io/oauth/token?client_id=%s&client_secret=%s&code=%s&grant_type=authorization_code&redirect_uri=%s",
+                        clientId, clientSecret, authCode, redirectUri
+                ),
+                null,
+                String.class
         );
     }
 }
